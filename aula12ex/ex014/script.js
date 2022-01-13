f009e3d87641e0da661219be6bd5be65cf904e31
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2022-01-13 16:07:20 -0300

    exercícios aula 12

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -2,14 +2,20 @@ function carregar(){
     var msg = window.document.querySelectorAll('div')[0]
     var img = window.document.querySelector('img')
     var data = new Date()
-    var hora = 9
-    msg.innerHTML = `Agora são ${hora} horas`
+    var hora = data.getHours()
+    var minu = data.getMinutes()
+    if (minu < 10) {
+        minu = '0' + minu 
+    }
+    msg.innerHTML = `Agora são <strong>${hora}:${minu}</strong> horas`
     if (hora >= 0 && hora < 12) {
         img.src = 'fotomanha.png'
-        document.body.style.background = 'blue linear-gradient(#5E68FF , #5CFF4D) center center / cover no-repeat fixed' 
+        document.body.style.background = 'green linear-gradient(#1F8C3A , #5CFF4D) center center / cover no-repeat fixed' 
     } else if (hora >= 12 && hora <= 18) {
         img.src = 'fototarde.png'
+        document.body.style.background = 'yellow linear-gradient(#EBA400, #F7F700) center center / cover no-repeat fixed' 
     } else {
         img.src = 'fotonoite.png'
+        document.body.style.background = 'darkblue linear-gradient(#190000, #090057) center center / cover no-repeat fixed' 
     }
 }
