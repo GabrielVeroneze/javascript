acd5930c64a70a91b6c2aeee7213124f36b2ae46
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2022-01-20 16:10:14 -0300

    exercicio 16

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,14 +1,40 @@
+bt.addEventListener('click', contar)
+
 function contar() {
-    var res = document.querySelector('div#res')
-    var numI = document.querySelector('input#Ini').value
-    var numF = document.querySelector('input#Fim').value
-    var pass = document.querySelector('input#Pas').value
-    
-    
-    if (numI <= numF) {
-        for (numI; Cont <= numF; i += pass) {
-            res.innerHTML = `${numI}`
+
+    var numI = document.getElementById('Ini')
+    var numF = document.getElementById('Fim')
+    var pass = document.getElementById('Pas')
+    var res = document.getElementById('res')
+
+    if (numI.value.length == 0 || numF.value.length == 0) {
+        
+        res.innerHTML = 'Impossível contar!'
+
+    } else if (pass.value == 0) {
+
+        window.alert('Passo inválido! Considerando PASSO 1')
+
+    } else { 
+
+        res.innerHTML = 'Contando:'
+        var inicio = Number(numI.value)
+        var fim = Number(numF.value)
+        var passo = Number(pass.value)
+        
+        if (inicio < fim) { // Contagem Crescente
+            
+            for (var c = inicio; c <= fim; c = c + passo) {
+                res.innerHTML += `${c}-`
+            }
+
+        } else { // Contagem Decrescente
+
+            for (var c2 = inicio; c2 >= fim; c2 = c2 - passo) {
+                res.innerHTML += `${c2}-`
+            }
         }
 
-    }
+    } 
+    
 }
