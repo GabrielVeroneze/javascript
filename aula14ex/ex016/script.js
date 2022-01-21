e53b15dc26f75fd22723f4faa34f903ca79b22fe
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2022-01-21 15:24:24 -0300

    ex016

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -9,32 +9,36 @@ function contar() {
 
     if (numI.value.length == 0 || numF.value.length == 0) {
         
-        res.innerHTML = 'Impossível contar!'
-
-    } else if (pass.value == 0) {
-
-        window.alert('Passo inválido! Considerando PASSO 1')
-
+        res.innerHTML = 'Você esqueceu de digitar um valor. Tente novamente!'
+    
     } else { 
-
-        res.innerHTML = 'Contando:'
+          
         var inicio = Number(numI.value)
         var fim = Number(numF.value)
         var passo = Number(pass.value)
         
+        res.innerHTML = 'Contando: <br>'
+
+        if (passo <= 0) {
+            
+            window.alert('Passo inválido! Considerando Passo 1')
+
+            passo = 1
+        }
+
         if (inicio < fim) { // Contagem Crescente
             
-            for (var c = inicio; c <= fim; c = c + passo) {
-                res.innerHTML += `${c}-`
+            for (var c = inicio; c <= fim; c += passo) {
+                res.innerHTML += `${c} \u{1F449} `
             }
 
         } else { // Contagem Decrescente
 
-            for (var c2 = inicio; c2 >= fim; c2 = c2 - passo) {
-                res.innerHTML += `${c2}-`
+            for (var c2 = inicio; c2 >= fim; c2 -= passo) {
+                res.innerHTML += `${c2} \u{1F449} `
             }
         }
-
+        res.innerHTML += ` \u{1F3C1} `
     } 
     
 }
