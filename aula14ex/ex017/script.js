bc07a0d4359f7be4b234b6e56aa35f02149c1848
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2022-01-22 16:06:36 -0300

    ex017

---
+++ aula14ex/ex017/script.js
@@ -0,0 +1,16 @@
+botao.addEventListener('click', clicar)
+function clicar() { 
+    var Num = document.getElementById('txtn')
+    var tab = document.getElementById('seltab')
+    if (Num.value.length == 0) {
+        window.alert('Por favor, digite um número!')
+    } else {
+        var N = Number(Num.value)
+        tab.innerHTML = ''
+        for (var c = 0; c < 11; c += 1) {  
+            var item = document.createElement('option')
+            item.text = `${N} x ${c} = ${N*c}` 
+            tab.appendChild(item)
+        }
+    }
+}
