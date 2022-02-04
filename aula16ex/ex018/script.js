85bd3034715a044c6e7699d426391e3c6b76a46b
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2022-02-04 15:42:41 -0300

    fim

--- aula16ex/ex018/script.js
+++ aula16ex/ex018/script.js
@@ -1,11 +1,13 @@
 Adic.addEventListener('click', adic)
 var Valor = []
 let apag = document.getElementById('Numero')
+var Res = document.querySelector('div#res')
 
 function adic(){
     let Num = Number(document.getElementById('Numero').value)
     let Lis = document.getElementById('lista')
     let Com = Valor.includes(Num)
+
     if (Num < 1 || Num > 100 || Com == true) {
         window.alert('Valor inválido ou já encontrado na lista.')
     } else {
@@ -13,36 +15,42 @@ function adic(){
         item.text = `Valor ${Num} adicionado.`
         Lis.appendChild(item)
         Valor.push(Num)
+        Res.innerHTML = ''
     }
     apag.value = ''
     apag.focus()
 }
 
 Final.addEventListener('click', final)
-var Res = document.querySelector('div#res')
+
 
 function final(){
+    if (Valor.length == 0) {
+        window.alert('Adicione valores antes de finalizar.')
+    } else {
+        Res.innerHTML = ''
 
-    Res.innerHTML += `Ao todo, temos ${Valor.length} números cadastrados.`
+        Res.innerHTML += `<p>Ao todo, temos ${Valor.length} números cadastrados.</p>`
 
-    Array.max = function(array) {
-        return Math.max.apply(Math, array);
-    };  
-    Res.innerHTML += `<br>O maior valor informado foi ${Array.max(Valor)}.`
+        Array.max = function(array) {
+            return Math.max.apply(Math, array);
+        };  
+        Res.innerHTML += `<p>O maior valor informado foi ${Array.max(Valor)}.</p>`
 
-    Array.min = function(array) {
-        return Math.min.apply(Math, array);
-    };
-    Res.innerHTML += `<br>O menor valor informado foi ${Array.min(Valor)}.`
-    
-    let soma = 0
-    for (let i = 0; i < Valor.length; i++) {
-        soma = soma + Valor[i]    
+        Array.min = function(array) {
+            return Math.min.apply(Math, array);
+        };
+        Res.innerHTML += `<p>O menor valor informado foi ${Array.min(Valor)}.</p>`
+        
+        let soma = 0
+        for (let i = 0; i < Valor.length; i++) {
+            soma = soma + Valor[i]    
+        }
+        Res.innerHTML += `<p>Somando todos os valores, temos ${soma}.</p>`
+
+        let media = soma / Valor.length
+        Res.innerHTML += `<p>A média dos valores digitados é ${media}.</p>`
     }
-    Res.innerHTML += `<br>Somando todos os valores, temos ${soma}.`
-
-    let media = soma / Valor.length
-    Res.innerHTML += `<br>A média dos valores digitados é ${media}.`
 }
 
 /* OUTRA FORMA DE DESCOBRIR O MAIOR VALOR
